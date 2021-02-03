82c6375e1748176453702653e30a98d453cc11d8
Loo Tung Lun <tung.lun.loo@intel.com>
2021-02-03 11:15:54 +0800

    fix the 1 and 2 configurations

--- patchtsn.py
+++ patchtsn.py
@@ -6,7 +6,7 @@ with open(sys.argv[1], 'r+b') as fh:
     fh.seek(0x2b1)  #offset for TSN 0 and TSN 1 config
 
     config = 0 # both 1G
-    # config = 1 # TSN 0 1 G, TSN 1 2.5G 
-    # config = 2 # TSN 0 2.5G, TSN 1 1G
+    # config = 1 # TSN 0 2.5G, TSN 1 1G
+    # config = 2 # TSN 0 1G,   TSN 1 2.5G
     # config = 3 # TSN 0 2.5G, TSN 1 2.5G
     fh.write(((config << 4) + 1).to_bytes(1, byteorder='big'))
